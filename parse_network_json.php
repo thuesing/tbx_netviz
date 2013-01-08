1a836370df6a111d05b40db9d70d6bc3a50843ad
thuesing <t.huesing@gmx.de>
2013-01-08 07:03:15 +0100

    pseudocode for network json parser

---
+++ parse_network_json.php
@@ -0,0 +1,32 @@
+<?php
+
+# for each expert
+
+	# foreach term in policy area
+
+		# schreibe term
+			# nach term dict  
+			# "nodes": [ {name, id, url, count} , .. ]
+	
+
+		# screibe
+		# "links": [ {source, target} , .. ]
+
+
+# foreach in term dict nodes
+
+$node_ids = taxonomy_select_nodes(#term->id);
+$res = get nodes for $node_ids
+
+foreach $res
+  if not expert
+  	unset from res
+
+
+term dict->nodes->name count = $res->count
+
+
+
+.
+
+
